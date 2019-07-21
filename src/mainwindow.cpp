ee759a3f6bc2b496324df6f42746ca1c6ea1482b
Aleksey <sarlift@mail.ru>
2019-07-21 23:39:13 +0400

    Подредактировал модель - представление Signed-off-by: Aleksey <sarlift@mail.ru>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,13 +6,19 @@
 
 MainWindow::MainWindow(QWidget *parent){
     this->setParent(parent);
-    FileReader file("D:\\Project\\ParserFile\\parserFile\\tlm\\2019_07_01_14-16-31.tlm");
+    FileReader file("D:\\Project\\ParserFile\\parserFile\\tlm\\2019_01_18_07-01-51.tlm");
     ParserTLM parserTlm(file.getHexString());
 
     //QTextEdit * text = new QTextEdit(parserTlm.getHexString());
     ModelTlm *model = new ModelTlm(*parserTlm.getBlocks());
     //model->setStringList(QStringList() <<"tfg"<<"tfbt"<<"tbrb"<<"tbrb"<<"tbrb"<<"tbrb");
     QTreeView * trv= new QTreeView();
+    trv->setAnimated(true);
+    trv->setWordWrap(true);
+    trv->setSortingEnabled(true);
+    trv->setItemsExpandable(true);
+    trv->setAllColumnsShowFocus(true);
+
     trv->setModel(model->getTlmModel());
     QHBoxLayout *blay = new QHBoxLayout(this);
     //blay->addWidget(text);
--- src/models/modeltlm.cpp
+++ src/models/modeltlm.cpp
@@ -2,22 +2,31 @@
 
 ModelTlm::ModelTlm(QList<BlockTlm> &tlmBlocks)
     :m_tlmBlocks(tlmBlocks){
-
-    this->model = new QStandardItemModel(m_tlmBlocks.size(),100);
+    int count;
+    this->model = new QStandardItemModel(m_tlmBlocks.size(),7);
     for(int row = 0; row < m_tlmBlocks.size();row++){
         QModelIndex indexName = model->index(row,0);
-        QModelIndex indexBoom = model->index(row,99);
+        QModelIndex indexBoom = model->index(row,1);
+        QModelIndex indexPuck;
         model->setData(indexName,tlmBlocks.at(row).name);
         model->setData(indexBoom,tlmBlocks.at(row).boom);
         model->insertRows(0,m_tlmBlocks.at(row).tlmPuckList.size(),indexName);
-        model->insertColumns(0,5,indexName);
+        model->insertColumns(0,7,indexName);
+        count = 0;
         for(int r = 0; r < m_tlmBlocks.at(row).tlmPuckList.size();r++){
-            model->setData(model->index(r,0,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).state);
-            model->setData(model->index(r,1,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).length);
-            model->setData(model->index(r,2,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.time);
-            model->setData(model->index(r,3,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.dev_type);
-            model->setData(model->index(r,4,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.inf_type);
-            model->setData(model->index(r,5,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.data);
+            model->setData(model->index(r,0,indexName),count++);
+            model->setData(model->index(r,1,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).state);
+            model->setData(model->index(r,2,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).length);
+            model->setData(model->index(r,3,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.time);
+            model->setData(model->index(r,4,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.dev_type);
+            model->setData(model->index(r,5,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.inf_type);
+            /*indexPuck = model->index(r,0,indexName);
+            model->insertRows(0,1,indexPuck);
+            model->insertColumns(0,5,indexPuck);
+            indexPuck = model->index(0,4,indexPuck);
+
+            model->setData(indexPuck,m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.data);*/
+            model->setData(model->index(r,6,indexName),m_tlmBlocks.at(row).tlmPuckList.at(r).dataPucket.data);
         }
     }
 }
